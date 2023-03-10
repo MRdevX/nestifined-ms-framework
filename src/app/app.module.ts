66c15d663db2620dd641076ea01f6df17f023ea1
Mahdi Rashidi <m8rashidi@gmail.com>
2023-03-10 19:37:24 +0330

    chore: add core module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,7 @@
+import { CoreModule } from './core/core.module';
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import appConfig from './config/app.config';
-import dbConfig from './config/db.config';
-import s2sConfig from './config/s2s.config';
-import sentryConfig from './config/sentry.config';
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({
-      isGlobal: true,
-      load: [appConfig, dbConfig, s2sConfig, sentryConfig],
-      envFilePath: ['.env'],
-    }),
-  ],
-  controllers: [],
-  providers: [],
+  imports: [CoreModule],
 })
 export class AppModule {}
---
+++ src/app/core/core.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import appConfig from '../config/app.config';
+import dbConfig from '../config/db.config';
+import s2sConfig from '../config/s2s.config';
+import sentryConfig from '../config/sentry.config';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [appConfig, dbConfig, s2sConfig, sentryConfig],
+      envFilePath: ['.env'],
+    }),
+  ],
+})
+export class CoreModule {}
