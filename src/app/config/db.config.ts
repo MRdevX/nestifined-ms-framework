c6477f1f7b21e6a1501d98d8809be00936dbcae5
Mahdi Rashidi <m8rashidi@gmail.com>
2023-03-10 19:48:36 +0330

    feat: add typeorm and db config

--- src/app/config/db.config.ts
+++ src/app/config/db.config.ts
@@ -1,14 +1,18 @@
 import { registerAs } from '@nestjs/config';
 
 export default registerAs('db', () => ({
-  host: process.env.DB_HOST || 'localhost',
+  url: process.env.DB_URL,
   type: process.env.DB_TYPE,
+  host: process.env.DB_HOST,
   port: parseInt(process.env.DB_PORT, 10) || 5432,
-  name: process.env.DB_NAME || 'demo',
-  user: process.env.DB_USER || 'postgres',
-  password: process.env.DB_PASS || 'postgres',
-  sync: process.env.DB_SYNC || false,
-  logging: process.env.DB_LOGGING || false,
-  maxConnections: parseInt(process.env.DATABASE_MAX_CONNECTIONS, 10) || 100,
-  sslEnabled: process.env.DATABASE_SSL_ENABLED === 'true',
+  password: process.env.DB_PASSWORD,
+  name: process.env.DB_NAME,
+  username: process.env.DB_USERNAME,
+  synchronize: process.env.DB_SYNCHRONIZE === 'true',
+  maxConnections: parseInt(process.env.DB_MAX_CONNECTIONS, 10) || 100,
+  sslEnabled: process.env.DB_SSL_ENABLED === 'true',
+  rejectUnauthorized: process.env.DB_REJECT_UNAUTHORIZED === 'true',
+  ca: process.env.DB_CA,
+  key: process.env.DB_KEY,
+  cert: process.env.DB_CERT,
 }));
