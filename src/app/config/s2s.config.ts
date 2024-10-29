822aadfd07c52c476a007c1ef12f4bdbd29342aa
Mahdi Rashidi <m8rashidi@gmail.com>
2024-10-29 19:11:05 +0300

    feat: add env support for rabbitmq in s2s envs

--- src/app/config/s2s.config.ts
+++ src/app/config/s2s.config.ts
@@ -3,13 +3,26 @@ import { Transport } from '@nestjs/microservices';
 import Joi from 'joi';
 
 export const s2sConfigSchema = Joi.object({
-  S2S_TRANSPORT: Joi.number().default(Transport.REDIS),
+  S2S_TRANSPORT: Joi.string().valid('REDIS', 'RABBITMQ').default('REDIS'),
   S2S_REDIS_URL: Joi.string().default('redis://localhost:6379'),
+  S2S_RABBITMQ_URL: Joi.string().default('amqp://localhost:5672'),
+  S2S_RABBITMQ_QUEUE: Joi.string().default('default_queue'),
 });
 
-export default registerAs('s2s', () => ({
-  transport: parseInt(process.env.S2S_TRANSPORT, 10) || Transport.REDIS,
-  options: {
-    url: process.env.S2S_REDIS_URL || 'redis://localhost:6379',
-  },
-}));
+export default registerAs('s2s', () => {
+  const transport = process.env.S2S_TRANSPORT || 'REDIS';
+  const options =
+    transport === 'RABBITMQ'
+      ? {
+          urls: [process.env.S2S_RABBITMQ_URL || 'amqp://localhost:5672'],
+          queue: process.env.S2S_RABBITMQ_QUEUE || 'default_queue',
+        }
+      : {
+          url: process.env.S2S_REDIS_URL || 'redis://localhost:6379',
+        };
+
+  return {
+    transport: transport === 'RABBITMQ' ? Transport.RMQ : Transport.REDIS,
+    options,
+  };
+});
