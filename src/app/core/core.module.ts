78c489e214acce2ca6b144b1996d878d815c7dc7
Mahdi Rashidi <m8rashidi@gmail.com>
2024-10-29 19:49:28 +0300

    fix: remove validations for now

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,10 +1,9 @@
-import * as Joi from 'joi';
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
-import appConfig, { appConfigSchema } from '../config/app.config';
-import dbConfig, { dbConfigSchema } from '../config/db.config';
-import s2sConfig, { s2sConfigSchema } from '../config/s2s.config';
-import sentryConfig, { sentryConfigSchema } from '../config/sentry.config';
+import sentryConfig from '@root/app/config/sentry.config';
+import appConfig from '@root/app/config/app.config';
+import dbConfig from '@root/app/config/db.config';
+import s2sConfig from '@root/app/config/s2s.config';
 
 @Module({
   imports: [
@@ -12,12 +11,12 @@ import sentryConfig, { sentryConfigSchema } from '../config/sentry.config';
       isGlobal: true,
       load: [appConfig, dbConfig, s2sConfig, sentryConfig],
       envFilePath: ['.env'],
-      validationSchema: Joi.object({
-        ...appConfigSchema.describe().keys,
-        ...dbConfigSchema.describe().keys,
-        ...s2sConfigSchema.describe().keys,
-        ...sentryConfigSchema.describe().keys,
-      }),
+      // validationSchema: Joi.object({
+      //   ...appConfigSchema.describe().keys,
+      //   ...dbConfigSchema.describe().keys,
+      //   ...s2sConfigSchema.describe().keys,
+      //   ...sentryConfigSchema.describe().keys,
+      // }),
     }),
   ],
 })
